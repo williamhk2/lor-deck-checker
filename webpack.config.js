c7a7a5bfe9b66481e1de6f2074f94539d74ba14a
William Pereira <william.hk@hotmail.com>
2021-03-07 11:40:49 -0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require( 'path' );
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'main.js',
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    }
+};
